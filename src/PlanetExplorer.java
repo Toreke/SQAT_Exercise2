e757d6b7643afb49d06affbad12fd4c96b73b78e
somename <someemail>
2016-10-19 16:31:23 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,7 +1,7 @@
 
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
-// ID:
-// Finish time:
+// ID: 149
+// Finish time: 16:30
 
 public class PlanetExplorer {
 	private String obstacle = null;
